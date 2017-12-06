f34f49f5905b38de8bb93fab7c71f6d8a2be4cd3
Paul Volavsek <paul.volavsek@gmail.com>
2017-12-06 17:15:14 +0100

    update greenbox.

--- greenbox.m
+++ greenbox.m
@@ -2,10 +2,23 @@ function [ Y ] = greenbox(G, B)
     rc = G(:,:,1);
     gc = G(:,:,2);
     bc = G(:,:,3);
-    mask = (rc < 50) & (bc < 50) & (gc > 50);
-    rc = rc.*uint8(mask);
-    gc = gc.*uint8(mask);
-    bc = bc.*uint8(mask);
-    Y = [rc gc gc];
+    
+    N_G = double(G)/255;
+    mask = N_G(:,:,2).*(N_G(:,:,2)-N_G(:,:,1)).*(N_G(:,:,2)-N_G(:,:,3));
+    % imshow(mask)
+    mask = mask > 0.1*mean(mask(mask>0));
+    
+    g_rc = G(:,:,1).*uint8(~mask);
+    g_gc = G(:,:,2).*uint8(~mask);
+    g_bc = G(:,:,3).*uint8(~mask);
+    
+    b_rc = B(:,:,1).*uint8(mask);
+    b_gc = B(:,:,2).*uint8(mask);
+    b_bc = B(:,:,3).*uint8(mask);
+    
+    
+    Y(:,:,1) = g_rc + b_rc;
+    Y(:,:,2) = g_gc + b_gc;
+    Y(:,:,3) = g_bc + b_bc;
 end
 
