7145d995f53e0792288e8562cb9b249926d5dd4a
Paul Volavsek <paul.volavsek@gmail.com>
2017-12-14 13:08:41 +0100

    update a3.

--- colorin.m
+++ colorin.m
@@ -1,8 +1,25 @@
 function [Y] = colorin(I)
-    Y = uint8(zeros(size(I)));
-    Y(I = 0,1)  = 20;
-    Y(I = 85)  = 20;
-    Y(I = 170)  = 20;
-    Y(I = 255)  = 20;
+    rc = uint8(zeros(size(I)));
+    gc = uint8(zeros(size(I)));
+    bc = uint8(zeros(size(I)));
+    
+    red_mask = I == 0;
+    green_mask = I == 85;
+    yellow_mask = I == 170;
+    cyan_mask = I == 255;<
+    
+    rc(red_mask) = 255;
+    
+    gc(green_mask) = 255;
+    
+    rc(yellow_mask) = 255;
+    gc(yellow_mask) = 255;
+
+    gc(cyan_mask) = 255;
+    bc(cyan_mask) = 255;
+    
+    Y(:,:,1) = rc;
+    Y(:,:,2) = gc;
+    Y(:,:,3) = bc;
 end
 
